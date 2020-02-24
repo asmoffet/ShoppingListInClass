06f0c48200fbf274cee41fc434f687d2494ac682
Alex Moffet <asmoffet@users.campus>
2020-02-24 17:12:56 -0600

    in class work

---
+++ Shopping List/ShoppingListData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Shopping_List
+{
+    public class ShoppingListData: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        List<string> items = new List<string>();
+        public IEnumerable<string> Items => items.ToArray();
+
+        public void Add(string Item)
+        {
+            items.Add(Item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+        public void Remove(string Item)
+        {
+            items.Remove(Item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+    }
+}
---
+++ Shopping List/addItemControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shopping_List
+{
+    /// <summary>
+    /// Interaction logic for addItemControl.xaml
+    /// </summary>
+    public partial class addItemControl : UserControl
+    {
+        public addItemControl()
+        {
+            InitializeComponent();
+            addButton.Click += addButton_Click;
+        }
+
+       protected void addButton_Click(object sender, RoutedEventArgs args)
+        {
+            if(DataContext is ShoppingListData data)
+            {
+                data.Add(ItemName.Text);
+                ItemName.Text = "";
+
+            }
+        }
+    }
+}
